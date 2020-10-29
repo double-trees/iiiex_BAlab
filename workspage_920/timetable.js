e05adcdd5f26494a554c1d5a1147b05110372473
double-trees <wide.high.double.trees@gmail.com>
2020-10-30 05:21:26 +0900

    10/30発表用

--- workspage_920/timetable.js
+++ workspage_920/timetable.js
@@ -14,7 +14,7 @@ function makeTable(data, tableId){
             if(i==0){
                 cell.style.backgroundColor = "#bbb"; // ヘッダ行
             }else{
-                cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
+                cell.style.backgroundColor = "#f0f0f0"; // ヘッダ行以外
             }
         }
     }
@@ -22,11 +22,17 @@ function makeTable(data, tableId){
     document.getElementById(tableId).appendChild(table);
 }
 window.onload = function(){ 
-// 表のデータ
-var data = [["実験説明", 12, 13],
-            [21, 22, 23],
-            [31, 32, 33],
-            [41, 42, 43]];
+    var now = new Date();
+    // 表のデータ
+
+var data = [["日","時間", "内容"],
+            ["ここに", "時間割を", "載せます"],
+            ["まだ決まってないので","時刻(UTC)を", "載せときます"],
+            [now.getUTCMonth()+1,"月", "あ"],
+            [now.getUTCDate(),"日" , "い"],
+            [now.getUTCHours(),"時" , "う"],
+            [now.getUTCMinutes(),"分" , "え"],
+            [now.getUTCSeconds(),"秒" , "お"]];
 
 // 表の動的作成
 makeTable(data,"table");
